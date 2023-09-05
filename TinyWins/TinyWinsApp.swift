dc20ebe325bf989bf31dba58c9ae287dcc83a868
Marvin Kome <marvin.kome@movinga.com>
2023-09-05 18:47:08 +0200

    Initial Commit

---
+++ TinyWins/TinyWinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyWinsApp.swift
+//  TinyWins
+//
+//  Created by Marvin Kome on 05.09.23.
+//
+
+import SwiftUI
+
+@main
+struct TinyWinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
